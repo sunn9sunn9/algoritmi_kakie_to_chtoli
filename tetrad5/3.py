16a841056640cd5c54b87ac66752437345730a0d
sunn9sunn9 <134864336+sunn9sunn9@users.noreply.github.com>
2023-06-07 13:56:45 +0300

    Create 3.py

---
+++ tetrad5/3.py
@@ -0,0 +1,18 @@
+plaintext = "This is a secret message."
+substitution = {
+    'a': 'z', 'b': 'y', 'c': 'x', 'd': 'w', 'e': 'v',
+    'f': 'u', 'g': 't', 'h': 's', 'i': 'r', 'j': 'q',
+    'k': 'p', 'l': 'o', 'm': 'n', 'n': 'm', 'o': 'l',
+    'p': 'k', 'q': 'j', 'r': 'i', 's': 'h', 't': 'g',
+    'u': 'f', 'v': 'e', 'w': 'd', 'x': 'c', 'y': 'b',
+    'z': 'a', ' ': ' '
+}
+
+ciphertext = ""
+for char in plaintext:
+    if char.lower() in substitution:
+        ciphertext += substitution[char.lower()]
+    else:
+        ciphertext += char
+
+print("The encrypted text is:", ciphertext)
